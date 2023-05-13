6c899d85817a5a8c5b37ff647fa2c9d38b02c732
sanjay khatri <sanjaykhatri906@gmail.com>
2023-05-13 17:47:05 +0545

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hlow world</header>
+      <header className="App-header">Hlow world from sanajy</header>
     </div>
   );
 }
